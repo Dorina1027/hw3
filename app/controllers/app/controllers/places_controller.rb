2c9f5b7e450a91590ddfff0081604b0fdef95f31
Dorina1027 <dongyunliu2027@u.northwestern.edu>
2026-02-24 07:31:14 -0600

    Update places_controller.rb

--- app/controllers/app/controllers/places_controller.rb
+++ app/controllers/app/controllers/places_controller.rb
@@ -1,1 +1,29 @@
+class PlacesController < ApplicationController
 
+  def index
+    @places = Place.all
+    render({ :template => "places/index" })
+  end
+
+  def show
+    @place = Place.find(params.fetch("id"))
+    render({ :template => "places/show" })
+  end
+
+  def new
+    @place = Place.new
+    render({ :template => "places/new" })
+  end
+
+  def create
+    @place = Place.new
+    @place.name = params.fetch("name")
+
+    if @place.save
+      redirect_to("/places/#{@place.id}")
+    else
+      redirect_to("/places")
+    end
+  end
+
+end

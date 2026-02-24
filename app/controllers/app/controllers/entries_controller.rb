e22083fe06c038d376b982e216683513b58a1bc3
Dorina1027 <dongyunliu2027@u.northwestern.edu>
2026-02-24 07:37:14 -0600

    Update entries_controller.rb

--- app/controllers/app/controllers/entries_controller.rb
+++ app/controllers/app/controllers/entries_controller.rb
@@ -1,1 +1,21 @@
+class EntriesController < ApplicationController
 
+  def new
+    @entry = Entry.new
+    @place_id = params.fetch("place_id")
+    render({ :template => "entries/new" })
+  end
+
+  def create
+    @entry = Entry.new
+    @entry.title = params.fetch("title")
+    @entry.description = params.fetch("description")
+    @entry.posted_on = params.fetch("posted_on")
+    @entry.place_id = params.fetch("place_id")
+
+    @entry.save
+
+    redirect_to("/places/#{@entry.place_id}")
+  end
+
+end
